0d2fed62a068a9f7209da18be16def4ef4924bd5
Damhee Kim <ekal.kdh@gmail.com>
2022-02-16 22:58:49 +0900

    useEffect

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function User({user, onRemove, changeColor}) {
+    // useEffect(() => {
+    //     // props -> state
+    //     // REST API
+    //     // setInterval, setTimeout
+    //     console.log('컴포넌트가 화면에 나타남')
+    //     return () => {
+    //         // clearInterval, clearTimeout
+    //         // 라이브러리 인스턴스 제거
+    //         console.log('컴포넌트가 화면에 사라짐')
+    //     }
+    // }, [])
+    // useEffect(함수, deps);
+    useEffect(() => {
+        console.log('user 값이 설정됨')
+        console.log(user)
+        return () => {
+            console.log('user 값이 바뀌기 전')
+            console.log(user)
+        }
+    }, [user]) // [user]의 등록, 삭제, 수정일때 출력됨
+
     return (
         <div>
             <strong 
